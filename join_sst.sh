6f8d736a99711a1edd66ca982211fcaa43ed063d
Michael Taylor <patternizer@gmail.com>
2019-02-13 14:55:47 +0000

    Change output path to: /gws/nopw/j04/fiduceo/Users/mtaylor/sst

--- join_sst.sh
+++ join_sst.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+x#!/bin/sh
 
-#FIDHOME=/home/laptop/Desktop/sst
-#FIDHOME=/group_workspaces/cems2/fiduceo/Users/mtaylor/sst
 FIDHOME=/gws/nopw/j04/fiduceo/Users/mtaylor/sst
-#INMYVENV=${FIDHOME}/inmyvenv.sh
-#GENERATE=${FIDHOME}/join_sst.py
-#MEM_REQ=10000 # in MB
-#MEM_MAX=10000 # in MB 
+INMYVENV=${FIDHOME}/inmyvenv.sh
+GENERATE=${FIDHOME}/join_sst.py
+MEM_REQ=10000 # in MB
+MEM_MAX=10000 # in MB 
 
 join(){
 
@@ -22,7 +20,6 @@ join(){
 	    do
                 JOBNAME="${SAT}${year}${month}${day}"
 		LOGDIR="$FIDHOME/${SAT}/${year}/${month}/${day}"	       
-
 		if [ -d "$LOGDIR" ]; then
 #		    python join_sst.py ${SAT} ${year} ${month} ${day}  
 #		    echo ${JOBNAME}
@@ -36,23 +33,23 @@ join(){
     
 }
 
-#join AVHRR06_G 1979 1982
-#join AVHRR07_G 1981 1985
-#join AVHRR08_G 1983 1985
-#join AVHRR09_G 1985 1992
-#join AVHRR10_G 1986 1991
-#join AVHRR11_G 1988 1995
-#join AVHRR12_G 1991 1998
-#join AVHRR14_G 1995 2002
-#join AVHRR15_G 1998 2010
-#join AVHRR16_G 2000 2010
-#join AVHRR17_G 2002 2010
-#join AVHRR18_G 2005 2017
-#join AVHRR19_G 2009 2017
-#join AVHRRMTA_G 2006 2017 
-#join AATSR 2002 2012
-#join ATSR1 1991 1997
-#join ATSR2 1995 2008
+join AVHRR06_G 1979 1982
+join AVHRR07_G 1981 1985
+join AVHRR08_G 1983 1985
+join AVHRR09_G 1985 1992
+join AVHRR10_G 1986 1991
+join AVHRR11_G 1988 1995
+join AVHRR12_G 1991 1998
+join AVHRR14_G 1995 2002
+join AVHRR15_G 1998 2010
+join AVHRR16_G 2000 2010
+join AVHRR17_G 2002 2010
+join AVHRR18_G 2005 2017
+join AVHRR19_G 2009 2017
+join AVHRRMTA_G 2006 2017 
+join AATSR 2002 2012
+join ATSR1 1991 1997
+join ATSR2 1995 2008
 wait
 
 
