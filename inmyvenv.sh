70a618a2f35ef4e976165c64fc268ad7f1e48d36
Michael Taylor <patternizer@gmail.com>
2019-02-13 00:00:04 +0000

    Update path to activate conda environment following re-install of Anaconda on /gws/nopw/j04/

--- inmyvenv.sh
+++ inmyvenv.sh
@@ -1,3 +1,4 @@
+#. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/etc/profile.d/conda.sh
 . /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/etc/profile.d/conda.sh
 export PATH=$PATH
 conda activate mike_cems
