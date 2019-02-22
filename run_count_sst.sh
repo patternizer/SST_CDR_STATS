2a46b6c3282cb269e9fe6721c871bbe05af2524f
Michael Taylor <patternizer@gmail.com>
2019-02-22 08:25:57 +0000

    Changed paths for new conda env at /gws/nopw/j04/

--- run_count_sst.sh
+++ run_count_sst.sh
@@ -19,46 +19,47 @@
 # A copy of the GNU General Public License should have been supplied along                
 # with this program; if not, see http://www.gnu.org/licenses/                             
 
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.2.sh
-echo python run_count_sst.py AVHRR07_G >> run.2.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.4.sh
-echo python run_count_sst.py AVHRR09_G >> run.4.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.6.sh
-echo python run_count_sst.py AVHRR11_G >> run.6.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.7.sh
-echo python run_count_sst.py AVHRR12_G >> run.7.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.8.sh
-echo python run_count_sst.py AVHRR14_G >> run.8.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.9.sh
-echo python run_count_sst.py AVHRR15_G >> run.9.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.10.sh
-echo python run_count_sst.py AVHRR16_G >> run.10.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.11.sh
-echo python run_count_sst.py AVHRR17_G >> run.11.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.12.sh
-echo python run_count_sst.py AVHRR18_G >> run.12.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.13.sh
-echo python run_count_sst.py AVHRR19_G >> run.13.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.14.sh
-echo python run_count_sst.py AVHRRMTA_G >> run.14.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.15.sh
-echo python run_count_sst.py AATSR >> run.15.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.16.sh
-echo python run_count_sst.py ATSR1 >> run.16.sh
-echo '. /group_workspaces/cems2/fiduceo/Users/mtaylor/anaconda3/bin/activate mike_cems' > run.17.sh
-echo python run_count_sst.py ATSR2 >> run.17.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.7.sh
+echo python run_count_sst.py AVHRR07_G >> run.7.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.9.sh
+echo python run_count_sst.py AVHRR09_G >> run.9.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.11.sh
+echo python run_count_sst.py AVHRR11_G >> run.11.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.12.sh
+echo python run_count_sst.py AVHRR12_G >> run.12.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.14.sh
+echo python run_count_sst.py AVHRR14_G >> run.14.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.15.sh
+echo python run_count_sst.py AVHRR15_G >> run.15.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.16.sh
+echo python run_count_sst.py AVHRR16_G >> run.16.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.17.sh
+echo python run_count_sst.py AVHRR17_G >> run.17.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.18.sh
+echo python run_count_sst.py AVHRR18_G >> run.18.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.19.sh
+echo python run_count_sst.py AVHRR19_G >> run.19.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.MA.sh
+echo python run_count_sst.py AVHRRMTA_G >> run.MA.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.AATSR.sh
+echo python run_count_sst.py AATSR >> run.AATSR.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.ATSR1.sh
+echo python run_count_sst.py ATSR1 >> run.ATSR1.sh
+echo '. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/bin/activate mike' > run.ATSR2.sh
+echo python run_count_sst.py ATSR2 >> run.ATSR2.sh
 
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.2.log < run.2.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.4.log < run.4.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.6.log < run.6.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.7.log < run.7.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.8.log < run.8.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.9.log < run.9.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.10.log < run.10.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.11.log < run.11.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.12.log < run.12.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.13.log < run.13.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.14.log < run.14.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.15.log < run.15.sh
 bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.16.log < run.16.sh
-bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.16.log < run.17.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.17.log < run.17.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.18.log < run.18.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.19.log < run.19.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.MA.log < run.MA.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.AATSR.log < run.AATSR.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.ATSR1.log < run.ATSR1.sh
+bsub -q short-serial -W24:00 -R rusage[mem=60000] -M 60000 -oo run.ATSR2.log < run.ATSR2.sh
+
